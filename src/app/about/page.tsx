a901d222745c90264dec2af7d0bf463fe3ca2a44
Tim Roty <timroty@users.noreply.github.com>
2024-03-26 22:13:49 -0500

    Email address about text changes

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -18,30 +18,32 @@ export default function About() {
               className="rounded-lg max-w-64"
             />
           </div>
-          <div className="flex flex-wrap w-full justify-center items-center mt-8">
-            <div className="max-w-screen-lg">
-              <p className="mt-4">
-                I am a software engineer currently living in Chicago and working
-                at Hudl on their Core Video team. I have 2+ years of experience
-                developing full-stack applications and my full work history can
-                be found on my LinkedIn. In addition to my professional work, I
-                enjoy creating personal projects and learning more about
-                software development. If you are interested in seeing more of my
-                work, check out my GitHub!
+          <div className="flex flex-wrap w-full justify-center items-center my-8">
+            <div className="max-w-screen-lg px-1">
+              <p className="my-3">
+                I&#39;m a software engineer currently living in Chicago and
+                working at Hudl on their video platform team. In addition to my
+                professional work, I enjoy creating personal projects and
+                learning more about software development. If you are interested
+                in seeing more of my work, check out my{" "}
+                <a target="_blank" href="https://github.com/timroty">
+                  GitHub
+                </a>
+                !
               </p>
-              <p className="mt-4">
+              <p className="my-3">
                 I graduated from the University of Nebraska-Lincoln&#39;s Raikes
                 School with a Bachelor&#39;s in Software Engineering and minors
                 in Business and Mathematics. Beyond software, I stay active and
                 competitive through running, weight lifting, and playing sports
                 like disc golf.
               </p>
-              <p className="mt-4">
+              <p className="my-3">
                 My goal for this site is to provide a creative outlet that
-                serves as a challenge in thoughtful expression, as well as
-                practice and learn full-stack development skills. It&#39;s an
-                ongoing project I hope to continue to build on. While you are
-                visiting, take a moment to check out my favorites and enjoy
+                serves as a fun challenge in thoughtful expression, as well as
+                to practice and learn full-stack development skills. It&#39;s an
+                ongoing project that I plan to continue to build on. While you
+                are visiting, take a moment to check out my favorites and enjoy
                 reading through my posts!
               </p>
             </div>
