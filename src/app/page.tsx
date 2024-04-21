350e2c1fe9e37a55c299cf086f53d56d1ecfab2e
Tim Roty <timroty@users.noreply.github.com>
2024-04-21 00:01:51 -0500

    Margin left on subtitle for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <NavigationBar></NavigationBar>
       <main className="container max-w-screen-xl">
         <h1 className="text-5xl font-bold mt-32">Tim Roty</h1>
-        <p className="text-md mt-4">Software Developer</p>
+        <p className="text-md mt-4 ml-1">Software Developer</p>
       </main>
     </>
   );
