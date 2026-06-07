89ff479806b981f0b578403745c4d7ebb30d97e5
Tim Roty <timroty@users.noreply.github.com>
2026-06-07 14:31:30 -0500

    Add Coffee pill

--- src/components/tag-pill.tsx
+++ src/components/tag-pill.tsx
@@ -18,6 +18,8 @@ export default function TagPill({ key, text }: Props) {
         return "bg-rose-500/30 border-rose-900/10 dark:bg-rose-400/40 dark:border-rose-700/20";
       case "Podcast Episode":
         return "bg-fuchsia-500/30 border-fuchsia-900/10 dark:bg-fuchsia-400/40 dark:border-fuchsia-700/20";
+      case "Coffee":
+        return "bg-amber-800/30 border-amber-950/10 dark:bg-amber-700/40 dark:border-amber-900/20";
       default:
         return "bg-gray-500/30 border-gray-900/10 dark:bg-gray-300/20 dark:border-gray-800/80";
     }
