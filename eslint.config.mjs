287a311dfe8e07282ac59108c9f7a0db7ad78da8
Tim Roty <timroty@users.noreply.github.com>
2026-09-19 14:09:19 -0500

    prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,10 +11,5 @@ export default defineConfig([
       semi: ["error", "always"],
     },
   },
-  globalIgnores([
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
 ]);
